9a2eb64ad78c7a2705aafa5601c915fbab2f90e3
Nik Markwell <nik@duckinator.net>
2014-04-13 00:27:21 -0400

    Make _case() a bit less gross.

--- test/control-statements.rb
+++ test/control-statements.rb
@@ -5,15 +5,7 @@ end
 _if(true, | | puts 'true!', | | puts 'false!')
 
 def _case(obj, hsh)
-  hsh.each do |k, v|
-    ret = _if(k === obj, | | v.call)
-
-    # Bullshit hack because I couldn't get _if() to play nice with returns.
-    # Wat @ having to wrap `return ret` in parenthesis.
-    ret && (return ret)
-  end
-
-  nil
+  hsh.find{|k, v| k === obj}.last.call
 end
 
 p _case(1,
